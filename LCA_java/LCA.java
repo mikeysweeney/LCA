25be0558c2402cb345a5bafcfeb151216d8352be
mikeysweeney <78807853+mikeysweeney@users.noreply.github.com>
2021-10-08 20:10:03 +0100

    build node

---
+++ LCA_java/LCA.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.list;
+
+class Node {
+  int data;
+  Node left;
+  Node right;
+
+  Node(int value) {
+    data = value;
+    left = right = null;
+  }
+}
