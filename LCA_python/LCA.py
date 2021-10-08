f6a332fbf14b5a2783f67ab81303dc6d2c6807eb
mikeysweeney <78807853+mikeysweeney@users.noreply.github.com>
2021-10-08 20:14:24 +0100

    build node

--- LCA_python/LCA.py
+++ LCA_python/LCA.py
@@ -1,1 +1,22 @@
-mmm
+#example to visualize 
+#           4
+#         /   \
+#        2      6
+#       / \    / \
+#      1   3  5   7
+import networkx as nx
+
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+    def find(self, x):
+        if self.key == x:
+            return True
+        elif x < self.key and self.left:
+            return self.left.find(x)
+        elif x > self.key and self.right:
+            return self.right.find(x)
+        return False
