e98c6c2a0d2eda2044d6439d7833e2ed2e6255ca
mikeysweeney <78807853+mikeysweeney@users.noreply.github.com>
2021-10-10 21:54:23 +0100

    Commiting LCA implementation and brushing up Nodes

--- LCA_java/LCA.java
+++ LCA_java/LCA.java
@@ -1,13 +1,71 @@
 import java.util.ArrayList;
-import java.util.list;
-
 class Node {
-  int data;
-  Node left;
-  Node right;
+		int data;
+		Node leftside, rightside;
+		ArrayList<Node> lcancestors;
 
-  Node(int value) {
-    data = value;
-    left = right = null;
-  }
+		Node(int value) {
+			data = value;
+			leftside = rightside = null;
+			lcancestors = null;
+		}
+	}
+
+public class LCA {
+	Node root;
+	boolean cycle = false;
+
+	Node findLCA(int n1, int n2) {
+		return findLCA(root, n1, n2);
+	}
+
+	Node findLCA(Node node, int n1, int n2) {
+		if (node == null)
+			return null;
+		else if (node.data == n1 || node.data == n2)
+			return node;
+		Node leftside = findLCA(node.leftside, n1, n2);
+		Node rightside = findLCA(node.rightside, n1, n2);
+		if (leftside != null && rightside != null)
+			return node;
+		else if (leftside != null)
+			return leftside;
+		else
+			return rightside;
+	}
+
+	public void addlcancestorsToNode(Node nodeA, Node nodeB) {
+		for (int i = 0; i < nodeA.lcancestors.size(); i++) {
+			if (nodeA.lcancestors.get(i) == nodeB)
+				cycle = true;
+			if (!nodeB.lcancestors.contains(nodeA.lcancestors.get(i)))
+				nodeB.lcancestors.add(nodeA.lcancestors.get(i));
+		}
+	}
+
+	public void addlcancestorsToNodeAtPosition(int position, Node nodeA, Node nodeB) {
+		for (int i = 0; i < nodeA.lcancestors.size(); i++)
+			if (!nodeB.lcancestors.contains(nodeA.lcancestors.get(i)))
+				nodeB.lcancestors.add(position, nodeA.lcancestors.get(i));
+	}
+
+	public void addToGraph(Node nodeA) {
+		nodeA.lcancestors = new ArrayList<Node>();
+		nodeA.lcancestors.add(nodeA);
+	}
+
+	Node findLCADAG(Node nodeA, Node nodeB) {
+		return findLCADAG(root, nodeA, nodeB);
+	}
+
+	Node findLCADAG(Node root, Node nodeA, Node nodeB) {
+		if (nodeA != null && nodeB != null && !cycle)
+			if (nodeA.lcancestors != null && nodeB.lcancestors != null) {
+				for (int i = 0; i < nodeB.lcancestors.size(); i++)
+					for (int j = 0; j < nodeA.lcancestors.size(); j++)
+						if (nodeB.lcancestors.get(i) == nodeA.lcancestors.get(j))
+							return nodeB.lcancestors.get(i);
+			} else return root;
+		return null;
+	}
 }
