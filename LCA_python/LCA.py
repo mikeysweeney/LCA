f9915348bc190c192f019d2c9c1873321aaa3739
mikeysweeney <78807853+mikeysweeney@users.noreply.github.com>
2021-10-26 13:25:00 +0100

    test

--- LCA_python/LCA.py
+++ LCA_python/LCA.py
@@ -4,8 +4,8 @@
 #        2      6
 #       / \    / \
 #      1   3  5   7
+#start dag
 import networkx as nx
-
 class Node:
     def __init__(self, key):
         self.key = key
@@ -49,18 +49,3 @@ print ("LCA(4, 5) = %d" %findLCA(root, 4, 5))
 print ("LCA(1, 2) = %d" %findLCA(root, 1, 2))
 print ("LCA(3, 2) = %d" %findLCA(root, 3, 2))
 print ("LCA(5, 7) = %d" %findLCA(root, 5, 7))
-
-graph = nx.DiGraph()
-graph.add_edges_from([(1, 2), (1, 3), (1, 4), (1, 5), (2, 4), (3, 4), (3, 5), (4, 5)])
-
-#   ____(1)_______
-#  V   /   \      \
-# (2)  |  (3)      |
-#  \   V  V  \     |
-#   -->(4)    |    |
-#        \    V   /
-#         -->(5)<-
-
-print ("LCA_DAG(2, 3) = %d" %nx.lowest_common_ancestor(graph, 2, 3))
-print ("LCA_DAG(2, 5) = %d" %nx.lowest_common_ancestor(graph, 2, 5))
-print ("LCA_DAG(1, 4) = %d" %nx.lowest_common_ancestor(graph, 1, 4))
